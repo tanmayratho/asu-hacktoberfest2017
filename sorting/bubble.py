647d693efc140452ddd0038404ecce1f713fdf02
tanmayratho <tkchariot@gmail.com>
2017-10-28 03:33:46 +0530

    bubble sort modified

--- sorting/bubble.py
+++ sorting/bubble.py
@@ -1,12 +1,18 @@
-def bubble(numbers):
-	n = len(numbers)
-	# traverse through all elements in the array
-	for i in range(n):
-		for j in range(0, n-i-1):
-			if numbers[j] > numbers[j+1]:
-				# TODO: swap numbers if element is greater
-				temp = numbers[j]
-				numbers[j] = numbers[j + 1]
-				numbers[j + 1] = temp
+n=int(input())
+A = []
+while n>0:
+    A.append(int(input()))
+    n-=1
 
-	return numbers;
+def bubblesort(A):
+ for i in range(len(A)):
+   for k in range(len(A)-1,i,-1):
+     if (A[k]<A[k-1]):
+       swap(A,k,k-1)
+ print(A)
+
+def swap(A,x,y):
+   tmp = A[x]
+   A[x] = A[y]
+   A[y] = tmp
+bubblesort(A)
